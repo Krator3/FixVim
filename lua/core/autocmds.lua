d3585fe8d419d0495cdc43ef4e1188d4ae57b488
Krator3 <work-drop3@yandex.ru>
2024-09-02 11:33:26 +0300

    Настроил терминал

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -9,3 +9,18 @@ vim.api.nvim_create_autocmd('BufHidden', {
     end
   end,
 })
+
+-- Настройка окна терминала
+vim.api.nvim_create_autocmd("TermOpen", {
+  pattern = "*",
+  callback = function()
+    -- Приписка _local задает параметр только для текущего буфера
+    vim.opt_local.number = false -- Отключаем нумерование строк
+    vim.opt_local.relativenumber = false -- Отключаем относительное нумерование строк
+    vim.opt_local.foldcolumn = "0" -- Оключаем сворачивание текста
+    vim.opt_local.signcolumn = "no" -- Отключаем показ меток ошибок, маркеров отладки и т.д.
+
+    vim.api.nvim_buf_set_keymap(0, "t", "<Esc>", "<C-\\><C-n>", { noremap = true, silent = true }) -- Esc переключает Terminal Mode в Normal Mode
+    vim.cmd("startinsert") -- Запуск терминала в Insert Mode при старте вместо дефолтного Normal Mode
+  end,
+})
